6714e6fc3e1cf316843799a794dc84ded1c87f6b
Manjunathmbn <44604424+Manjunathmbn@users.noreply.github.com>
2025-06-08 20:36:37 +0530

    Update Course.java

--- src/main/java/com/course/model/Course.java
+++ src/main/java/com/course/model/Course.java
@@ -3,6 +3,11 @@ package com.course.model;
 public class Course {
     private String id;
     private String name;
+    private String organizationId;
+    private String productModelRegionId;
+    private String status = "ACTIVE";
+    private String location="US";
+    private Audit audit;
 
     public Course() {
     }
@@ -27,4 +32,44 @@ public class Course {
     public void setName(String name) {
         this.name = name;
     }
+
+	public String getOrganizationId() {
+		return organizationId;
+	}
+
+	public void setOrganizationId(String organizationId) {
+		this.organizationId = organizationId;
+	}
+
+	public String getProductModelRegionId() {
+		return productModelRegionId;
+	}
+
+	public void setProductModelRegionId(String productModelRegionId) {
+		this.productModelRegionId = productModelRegionId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Audit getAudit() {
+		return audit;
+	}
+
+	public void setAudit(Audit audit) {
+		this.audit = audit;
+	}
 }
