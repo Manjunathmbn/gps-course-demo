fc3489a9d71c739c7276b28c299bc0503c87df3a
Manjunathmbn <44604424+Manjunathmbn@users.noreply.github.com>
2025-06-08 20:36:15 +0530

    Create Audit.java

---
+++ src/main/java/com/course/model/Audit.java
@@ -0,0 +1,37 @@
+package com.course.model;
+
+public class Audit {
+	private String dateCreated;
+	private String dateModified;
+	private String createdBy;
+	private String modifiedBy;
+	
+    public Audit() {
+    }
+    
+	public String getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(String dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	public String getDateModified() {
+		return dateModified;
+	}
+	public void setDateModified(String dateModified) {
+		this.dateModified = dateModified;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+}
